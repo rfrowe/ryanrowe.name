1661844a783ca355986bdc6b0213af0cc4cdf8c9
Ryan <ryanf.rowe@gmail.com>
2016-08-25 07:41:37 -0400

    Fully added materialize

--- public_html/files/common.php
+++ public_html/files/common.php
@@ -1,11 +1,16 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link href="/css/style.css" rel="stylesheet" type="text/css">
-    <?php readfile($_SERVER['DOCUMENT_ROOT'] . "/files/favicons.html") ?>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="/css/materialize.css" rel="stylesheet">
+
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="/files/js/materialize.min.js"></script>
     <script src="/files/js/analytics.js"></script>
     <script src="/files/js/scrollbar.js"></script>
+
+
+    <?php readfile($_SERVER['DOCUMENT_ROOT'] . "/files/favicons.html") ?>
 
     <!--[if lt IE 9]>
     <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
