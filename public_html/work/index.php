47c7820a77b594ad99f08bd37b72138043893d20
Ryan <ryanf.rowe@gmail.com>
2016-08-30 00:19:14 -0700

    Almost finished with work page, can't do any more until I have internet

--- public_html/work/index.php
+++ public_html/work/index.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="description" content="A record of my previous work experiences and projects that I have worked on such as web design and app development.">
-    <title>Ryan Rowe &mdash; Work</title>
+    <title>Work</title>
 
     <?php include($_SERVER['DOCUMENT_ROOT'] . "/files/common.php") ?>
     <link href="/css/work.css" rel="stylesheet" type="text/css">
 
     <script>
 	$(document).ready(function(e) {
-		$(".visit").click(function(e) {
+		/*$(".visit").click(function(e) {
             e.stopPropagation();
         });
         $('.toggle').click(function(){
@@ -28,79 +28,109 @@
 			    $(this).addClass("open");
 			}
 			$(this).siblings(".toggleable").slideToggle(300);
-    	});
+    	});*/
     });
 	</script>
 </head>
 
 <body>
-<div class="container">
-  <?php readfile($_SERVER['DOCUMENT_ROOT'] . "/files/header.html") ?>
-  <article class="content offset">
-  	<section class="main">
-    	<h1>Previous Work</h1>
-        <section class="dropshadow">
-        	<!--<section class="work">
-            	<div class="toggle trans">
-                    <button class="dropdown trans"></button>
-                    <h3>Cat Fax<a class="visit dropshadow" target="_blank" href="http://catfax.ml">Take a look</a></h3>
-                    <h4>Seattle, WA — 2015</h4>
+<?php readfile($_SERVER['DOCUMENT_ROOT'] . "/files/header.html") ?>
+<main class="grey">
+    <div class="container">
+        <h1>Previous Work</h1>
+        <ul class="collapsible" id="work" data-collapsible="accordion">
+            <li>
+                <div class="collapsible-header">
+                    <div class="row">
+                        <h5 class="col s9">Practice-It, University of Washington CSE</h5>
+                        <a href="http://practiceit.cs.washington.edu" class="btn right blue col s2">
+                            <span class="hide-on-med-and-down">Visit</span><!--<i class="material-icons">open_in_new</i>-->
+                        </a>
+                        <h6 class="col s12">Seattle, Washington — 2016 to Present</h6>
+                    </div>
                 </div>
-                <div class="toggleable">
-                CatFax is a cat fact delivery service. It delivers cat facts with automated SMS delivery. The application is written in Java and runs day and night to meet our subscriber&lsquo;s needs.
-                </div>
-            </section> -->
-            <section class="work">
-            	<div class="toggle trans">
-                    <button class="dropdown trans"></button>
-                    <h3>Practice-It, University of Washington CS Department <a href="http://practiceit.cs.washington.edu" class="visit dropshadow">Visit</a></h3>
-                    <h4>Seattle, Washington — 2016</h4>
-                </div>
-                <div class="toggleable">
-                    <p>Worked as part of a four-member faculty team to improve Practice-It, a website run by the University of Washington that is used by hundreds of thousands of people globally to practice and learn programming. Improved frontend usability as well as enhanced backend and added new features to both.</p>
-                    <ul>
+                <div class="collapsible-body grey lighten-5">
+                    <p class="flow-text">Worked as part of a four-member faculty team to improve Practice-It, a
+                        website run by the University of Washington that is used by hundreds of thousands of people
+                        globally to practice and learn programming. Improved frontend usability as well as enhanced
+                        backend and added new features to both.</p>
+                    <ul class="flow-text">
                         <li>Gained experience working on enterprise level software</li>
                         <li>Collaborated with other team members using version control software</li>
                         <li>Developed and improved frontend and backend interfaces</li>
                     </ul>
                 </div>
-            </section>
-        	<section class="work">
-            	<div class="toggle trans">
-                    <button class="dropdown trans"></button>
-                    <h3>Random Cube, CSE 390 HA <a class="visit dropshadow" href="/work/RandomCube/">Try it</a></h3>
-                    <h4>Seattle, WA — 2015</h4>
+            </li>
+            <li>
+                <div class="collapsible-header">
+                    <div class="row">
+                        <h5 class="col s9">Ohio Supercomputing Center Summer Institute</h5>
+                        <a href="/work/OSCSI/" class="btn right blue col s2">
+                            <span class="hide-on-med-and-down">Watch</span><!--<i class="material-icons">open_in_new</i>-->
+                        </a>
+                        <h6 class="col s12">Columbus, Ohio — 2012</h6>
+                    </div>
                 </div>
-                <div class="toggleable"><img src="/images/cube.png" alt="A randomly generated cube" id="cube"><p>In this class, we were charged with creating our own algorithmic art using a programming langage called Processing. I created a program that generates random formulae using the sin, cos, average, and multiplication functions. These functions always return values in the interval [-1,1]. The functions are generated at a recursive level of complexity. A level 1 formula may be <span class="code">cos(&pi;x)</span> while a level 3 may be <span class="code">(sin(&pi;y) * sin(&pi;x) + (cos(&pi;z) + cos(&pi;y)) / 2.0) / 2.0</span>. The program then uses three formulae to calclate RGB values for different (x,y,z) coordinates on a cube. The results look amazing and have incredible sinusoidal patterns.</p><div style="clear:both;"></div></div>
-            </section>
-        	<section class="work">
-            	<div class="toggle trans">
-                    <button class="dropdown trans"></button>
-                    <h3>Ohio Supercomputing Center Summer Institute <a class="visit dropshadow" href="/work/OSCSI/">Watch it</a></h3>
-                    <h4>Columbus, OH — 2012</h4>
+                <div class="collapsible-body grey lighten-5">
+                    <p class="flow-text">
+                        <img src="/images/oscsi.jpg" alt="Picture processing of Sun's corona" id="oscsi">
+                        I was one of 16 students selected to attend SI, a two­week residential program that gives
+                        gifted Ohio high school students entering their sophomore, junior or senior year project-based,
+                        hands­on learning. Working in small peer teams, we used supercomputers for practical
+                        applications such as solving complex science and engineering problems, conducting network
+                        forensics to catch hackers, studying the spread of the bird flu and designing computer games. My
+                        team performed image analysis of NASA satellite images using MATLAB in order to identify new
+                        comets and presented out findings to over 100 people.
+                    </p>
                 </div>
-                <div class="toggleable"><img src="/images/oscsi.jpg" alt="Picture processing of Sun's corona" id="oscsi"><p>I was one of 16 students selected to attend SI, a two ­week residential program that gives gifted Ohio high school students entering their sophomore, junior or senior year project-­based, hand-s­on learning. Working in small peer teams, we used supercomputers for practical applications such as solving complex science and engineering problems, conducting network forensics to catch hackers, studying the spread of the bird flu and designing computer games. My team performed image analysis of NASA satallite images using MATLAB in order to identify new comets and presented out findings to over 100 people.</p><div style="clear:both;"></div></div>
-            </section>
-            <section class="work">
-            	<div class="toggle trans">
-                    <button class="dropdown trans"></button>
-                    <h3>Webmaster, Unitarian Universalist Ohio-Meadville District <a href="/work/omdyac/" class="visit dropshadow">Visit</a></h3>
-                    <h4>Westlake, Ohio — 2011-2014</h4>
+            </li>
+            <li>
+                <div class="collapsible-header">
+                    <div class="row">
+                        <h5 class="col s9">Webmaster, Unitarian Universalist Ohio-Meadville District</h5>
+                        <a href="/work/omdyac/" class="btn right blue col s2">
+                            <span class="hide-on-med-and-down">Visit</span><!--<i class="material-icons">open_in_new</i>-->
+                        </a>
+                        <h6 class="col s12">Westlake, Ohio — 2011 to 2014</h6>
+                    </div>
                 </div>
-                <div class="toggleable">
-                    <img src="/images/omdyac.jpg" alt="Screenshot of the website">
-                    <p>As webmaster, was charged with maintaining website to provide information to an estimated 300 people in OH, PA, and WV for church youth group. During course of work utilized Adobe Dreamweaver to manage project and publish code.</p>
-                    <ul>
+                <div class="collapsible-body grey lighten-5">
+                    <p class="flow-text">
+                        <img class="responsive-img" src="/images/omdyac.jpg" alt="Screenshot of the website">
+                        As webmaster, was charged with maintaining website to provide information to an estimated 300
+                        people in OH, PA, and WV for church youth group. During course of work utilized Adobe
+                        Dreamweaver to manage project and publish code.</p>
+                    <ul class="flow-text">
                         <li>Completely redesigned website frontend and backend, requiring over 600 hours of work</li>
                         <li>Integrated PHP server-side scripting and MySQL to generate dynamic web content</li>
                         <li>Created administrative user interface using up-to-date password security and login session standards</li>
                     </ul>
                 </div>
-            </section>
-         </section>
-     </section>
-  </article> <!-- end .content -->
-  <?php include($_SERVER['DOCUMENT_ROOT'] . "/files/footer.php") ?>
-</div> <!-- end .container -->
+            </li>
+            <!--
+            <li>
+                <div class="collapsible-header">
+                    <div class="row">
+                        <h5 class="col s9">Work Title</h5>
+                        <a href="#" class="btn right blue col s2">
+                            <span class="hide-on-med-and-down">Visit</span><i class="material-icons">open_in_new</i>
+                        </a>
+                        <h6 class="col s12">Location — Years</h6>
+                    </div>
+                </div>
+                <div class="collapsible-body grey lighten-5">
+                    <p class="flow-text">
+                        Description
+                    <ul class="flow-text">
+                        <li>List Items/li>
+                    </ul>
+                </div>
+            </li>
+            -->
+        </ul>
+    </div>
+</main>
+<?php include($_SERVER['DOCUMENT_ROOT'] . "/files/footer.php") ?>
 </body>
+</html>
 </html>
