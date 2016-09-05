0cbf1d0b68f63b83914b00b89b1a7e229aaecaa5
Ryan <ryanf.rowe@gmail.com>
2016-09-05 00:48:37 -0700

    Added integrity hashes to external js libraries

--- public_html/files/common.php
+++ public_html/files/common.php
@@ -5,7 +5,9 @@
     <link href="/css/materialize.css" rel="stylesheet">
     <link href="/css/font-awesome.min.css" rel="stylesheet">
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"
+            integrity="sha384-rY/jv8mMhqDabXSo+UCggqKtdmBfd3qC2/KvyTDNQ6PcUJXaxK1tMepoQda4g5vB" crossorigin="anonymous">
+    </script>
     <script>window.jQuery || document.write('<script src="/files/js/jquery-2.2.4.min.js">\x3C/script>')</script>
     <script src="/files/js/materialize.min.js"></script>
     <script src="/files/js/analytics.js"></script>
@@ -19,5 +21,7 @@
     <?php readfile($_SERVER['DOCUMENT_ROOT'] . "/files/favicons.html") ?>
 
     <!--[if lt IE 9]>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"
+            integrity="sha384-qFIkRsVO/J5orlMvxK1sgAt2FXT67og+NyFTITYzvbIP1IJavVEKZM7YWczXkwpB" crossorigin="anonymous">
+    </script>
     <![endif]-->
